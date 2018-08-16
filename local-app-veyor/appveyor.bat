804f4a9390c51ae72312293d0bfd8f15d3e5cf4e
inno-tecth <hon.mirage@qq.com>
2018-08-16 02:29:43 -0700

    Update appveyor.bat

--- local-app-veyor/appveyor.bat
+++ local-app-veyor/appveyor.bat
@@ -3,23 +3,23 @@ set DEVENV_EXE="C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE\
 set VSDEVCMD_BAT="C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\Tools\vsvars32.bat"
 cd deps
 	:: zlib
-curl -fsSL -o zlib.tar.gz https://github.com/madler/zlib/archive/v1.2.8.tar.gz
+::curl -fsSL -o zlib.tar.gz https://github.com/madler/zlib/archive/v1.2.8.tar.gz
 7z x zlib.tar.gz -so | 7z x -si -ttar > nul
 move zlib-1.2.8 zlib
 cd zlib
-cmake . -DCMAKE_BUILD_TYPE=Release
-cmake --build .
+cmake .
+cmake --build . --config Release
 cd ..
 	:: libPNG
-curl -fsSL -o libpng.tar.gz http://prdownloads.sourceforge.net/libpng/libpng-1.6.28.tar.gz?download
+::curl -fsSL -o libpng.tar.gz http://prdownloads.sourceforge.net/libpng/libpng-1.6.28.tar.gz?download
 7z x libpng.tar.gz -so | 7z x -si -ttar > nul
 move libpng-1.6.28 libpng
 cd libpng
-cmake . -DCMAKE_BUILD_TYPE=Release -DZLIB_INCLUDE_DIR=..\zlib -DZLIB_LIBRARY=..\zlib\debug\zlibstaticd.lib
-cmake --build .
+cmake . -DZLIB_INCLUDE_DIR=..\zlib -DZLIB_LIBRARY=..\zlib\debug\zlibstaticd.lib
+cmake --build . --config Release
 cd ..
 	:: Berkeley DB - required by SASL
-curl -fsSL -o db-4.1.25.tar.gz http://download.oracle.com/berkeley-db/db-4.1.25.tar.gz
+::curl -fsSL -o db-4.1.25.tar.gz http://download.oracle.com/berkeley-db/db-4.1.25.tar.gz
 7z x db-4.1.25.tar.gz -so | 7z x -si -ttar > nul
 move db-4.1.25 db
 cd db\build_win32
@@ -28,7 +28,7 @@ echo using devenv %DEVENV_EXE%
 msbuild /p:Configuration=Release db_dll.vcxproj
 cd ..\..
 	:: Cyrus SASL
-curl -fsSL -o cyrus-sasl-2.1.26.tar.gz ftp://ftp.cyrusimap.org/cyrus-sasl/cyrus-sasl-2.1.26.tar.gz
+::curl -fsSL -o cyrus-sasl-2.1.26.tar.gz ftp://ftp.cyrusimap.org/cyrus-sasl/cyrus-sasl-2.1.26.tar.gz
 7z x cyrus-sasl-2.1.26.tar.gz -so | 7z x -si -ttar > nul
 move cyrus-sasl-2.1.26 sasl
 cd sasl
@@ -41,8 +41,8 @@ cd ..
 cd ..
 mkdir build
 cd build 
-cmake .. -DZLIB_INCLUDE_DIR=..\deps\zlib -DZLIB_LIBRARY=..\deps\zlib\release\zlibstaticd.lib -DPNG_PNG_INCLUDE_DIR=..\deps\libpng -DPNG_LIBRARY=..\deps\libpng\release\libpng16_staticd.lib -D SASL2_INCLUDE_DIR=c:\cmu\include -D LIBSASL2_LIBRARIES=c:\cmu\lib\libsasl.lib ..
-cmake --build .
+cmake .. -DZLIB_INCLUDE_DIR=..\deps\zlib -DZLIB_LIBRARY=..\deps\zlib\release\zlibstatic.lib -DPNG_PNG_INCLUDE_DIR=..\deps\libpng -DPNG_LIBRARY=..\deps\libpng\release\libpng16_static.lib -D SASL2_INCLUDE_DIR=c:\cmu\include -D LIBSASL2_LIBRARIES=c:\cmu\lib\libsasl.lib ..
+cmake --build . --config Release
 ctest -C Release --output-on-failure
 
 pause
